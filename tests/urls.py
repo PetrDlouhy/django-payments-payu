99edfbd1b1a8ba2f27e26aa5da0888e02c662a50
Petr Dlouhý <petr.dlouhy@email.cz>
2022-03-16 10:11:15 +0100

    fix tests in Django 4.0

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'payments^', include('payments.urls')),
+    path('admin/', admin.site.urls),
+    path('payments/', include('payments.urls')),
 ]
